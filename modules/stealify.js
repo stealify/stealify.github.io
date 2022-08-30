26a5552b128ab83333c7c503082df6aa1f73f22b
Frank Lemanschik <frank@lemanschik.com>
2022-08-30 10:44:11 +0200

    Create stealify.js
    test it

---
+++ modules/stealify.js
@@ -0,0 +1,18 @@
+// dependencies
+const md = await import('https://unpkg.com/browse/markdown-wasm@1.2.0/dist/markdown.es.js')
+
+
+/**
+ * some stealify style code to load .md files in the browser and render them so we 
+ * do not need to build always a new page when we change a file or run a action
+ * it is a waist of time. 
+*/
+const loadIndexMdfromUrlHash = ({ newURL, oldURL }) => {
+
+  window["markdown"].ready.then(markdown => {
+    console.log(markdown.parse("# hello\n*world*"))
+  })
+
+  
+}
+window.addEventListener("hashchange", loadIndexMdfromUrlHash, false);
