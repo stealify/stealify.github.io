a9a2d0345ac474b9e67190e11534f85744027549
Frank Lemanschik <frank@lemanschik.com>
2022-08-30 11:31:16 +0200

    Update stealify.js
    i should really do local development from now on

--- modules/stealify.js
+++ modules/stealify.js
@@ -1,16 +1,28 @@
 // dependencies
 //await import('https://unpkg.com/markdown-wasm@1.2.0/dist/markdown.js')
-const markdown = window['markdown'];
-
-
+const markdown = await window['markdown'].ready.then(markdown);
+// We need to store that on load it is gone later it exists only on the first iteration
+const importMeta = import.meta.url;
+const isErrorPage = new URL(importMeta).searchParams.get('page') === "404";
+console.log('isErrorPage')
+window['stealify'] = { markdown, importMeta }
 /**
  * some stealify style code to load .md files in the browser and render them so we 
  * do not need to build always a new page when we change a file or run a action
  * it is a waist of time. 
 */
 const loadIndexMdfromUrlHash = ({ newURL, oldURL }) => {
-  markdown.ready.then(markdown => {
+  // fetch the newURL if 404 use the 404 handler directly on this page 
+  
     console.log(markdown.parse("# hello\n*world*"))
-  })
+  
+  
 }
 window.addEventListener("hashchange", loadIndexMdfromUrlHash, false);
+
+const handle = {
+  ["404"]() {
+    
+  }
+}
+
